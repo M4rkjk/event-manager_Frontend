c5304649a36338f977ecccdea654893feb78fc9f
Horvath Mark <horvath.mark1@students.jedlik.eu>
2025-06-07 15:02:09 +0200

    dataservice has been created with login

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  private apiUrl = 'http://127.0.0.1:8000/api';
+
+  constructor(private http: HttpClient) {}
+
+  login(email: string, password: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, { email, password });
+  }
+
+}
