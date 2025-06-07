1fd7014fac7d3cedb325dd95b35b3e4c9c0f0bcf
Horvath Mark <horvath.mark1@students.jedlik.eu>
2025-06-07 15:12:53 +0200

    event-list component has been created

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { DataService } from '../../services/data.service';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  email = '';
+  password = '';
+  errorMessage = '';
 
+  constructor(
+    private dataService: DataService,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  onSubmit() {
+    this.dataService.login(this.email, this.password).subscribe({
+      next: (response) => {
+        this.authService.setToken(response.token);
+        this.router.navigate(['/event-list']);
+      },
+      error: (err) => {
+        this.errorMessage = 'Login failed: Invalid credentials';
+      },
+    });
+  }
 }
