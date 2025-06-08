2446f7b94322fc9e9673b679b700f14dc71138b3
Horvath Mark <horvath.mark1@students.jedlik.eu>
2025-06-08 16:10:38 +0200

    Event-list component is working, dataService has been updated with get,update,delete event

--- src/app/components/event-list/event-list.component.ts
+++ src/app/components/event-list/event-list.component.ts
@@ -1,11 +1,90 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
+import { EventModel } from '../../models/event.model';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-event-list',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './event-list.component.html',
-  styleUrl: './event-list.component.css'
+  styleUrl: './event-list.component.css',
 })
-export class EventListComponent {
+export class EventListComponent implements OnInit {
+  events: EventModel[] = [];
+  isEditModalOpen = false;
+  selectedEvent: EventModel | null = null;
 
+  isDeleteModalOpen = false;
+  eventToDelete: EventModel | null = null;
+
+  errorMessage = '';
+
+  constructor(private dataService: DataService, router: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.refreshEvents();
+  }
+
+  refreshEvents(): void {
+    this.dataService.getEvents().subscribe({
+      next: (data) => {
+        this.events = data;
+      },
+      error: (err) => {
+        this.errorMessage = 'Failed to load events.';
+        console.error(err);
+      },
+    });
+  }
+
+  editEvent(event: EventModel) {
+    this.selectedEvent = { ...event };
+    this.isEditModalOpen = true;
+  }
+
+  deleteEvent(event: EventModel) {
+    this.eventToDelete = event;
+    this.isDeleteModalOpen = true;
+  }
+
+  confirmDelete() {
+    if (!this.eventToDelete) return;
+
+    this.dataService.deleteEvent(this.eventToDelete.id).subscribe({
+      next: () => {
+        this.refreshEvents();
+        this.closeDeleteModal();
+      },
+      error: (err) => {
+        console.error('Hiba a törlés során:', err);
+        this.errorMessage = 'Failed to delete event.';
+      },
+    });
+  }
+
+  closeDeleteModal() {
+    this.isDeleteModalOpen = false;
+    this.eventToDelete = null;
+  }
+
+  closeModal() {
+    this.isEditModalOpen = false;
+    this.selectedEvent = null;
+  }
+
+  saveChanges() {
+    if (!this.selectedEvent) return;
+
+    this.dataService.updateEvent(this.selectedEvent).subscribe({
+      next: () => {
+        this.closeModal();
+        this.refreshEvents();
+      },
+      error: (err) => {
+        console.error('Hiba a mentés során:', err);
+      },
+    });
+  }
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,17 +1,45 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+import { EventModel } from '../models/event.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class DataService {
+  export class DataService {
   private apiUrl = 'http://127.0.0.1:8000/api';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private authService: AuthService) {}
 
   login(email: string, password: string): Observable<any> {
     return this.http.post(`${this.apiUrl}/login`, { email, password });
   }
 
-}
+  getEvents(): Observable<EventModel[]> {
+    return this.http.get<EventModel[]>(`${this.apiUrl}/events`, {
+      headers: {
+        Authorization: `Bearer ${this.authService.getToken()}`,
+      },
+    });
+  }
+
+  updateEvent(event: EventModel): Observable<EventModel> {
+    return this.http.put<EventModel>(
+      `${this.apiUrl}/events/${event.id}`,
+      event,
+      {
+        headers: {
+          Authorization: `Bearer ${this.authService.getToken()}`,
+        },
+      }
+    );
+  }
+
+  deleteEvent(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/events/${id}`, {
+      headers: {
+        Authorization: `Bearer ${this.authService.getToken()}`
+      }
+    });
+}}
